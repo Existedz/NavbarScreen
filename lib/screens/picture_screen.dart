139b7a1b5b460f1f3c64b071c43c9ca6c71fa845
Existedz <129545234+Existedz@users.noreply.github.com>
2024-10-29 10:20:35 +0700

    Add files via upload

---
+++ lib/screens/picture_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import'package:flutter_masonry_view/flutter_masonry_view.dart';
+class PictureScreen extends StatelessWidget {
+PictureScreen({super.key});
+final _items = [
+'assets/images/Roblox1.png',
+];
+
+@override
+Widget build(BuildContext context) {
+return Padding(
+padding: const EdgeInsets.all(8.0),
+child: SingleChildScrollView(
+child: MasonryView(
+listOfItem: _items,
+numberOfColumn: 2,
+itemBuilder: (item) {
+return Image.asset(item);
+},
+),
+),
+);
+}
+}
